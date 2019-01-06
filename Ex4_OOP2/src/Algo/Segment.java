15e35de63d82ea6b0d6d24200e999f9a2176d1a6
shaynaor <44779960+shaynaor@users.noreply.github.com>
2019-01-06 11:17:33 +0200

    Added segment docs

--- Ex4_OOP2/src/Algo/Segment.java
+++ Ex4_OOP2/src/Algo/Segment.java
@@ -2,27 +2,33 @@ package Algo;
 
 import Geom.Point3D;
 
+/**
+ * This class represents a segment. a segment is a line between two points. it
+ * can be vertical or not.
+ * 
+ * @author Alex vaisman, Shay naor.
+ *
+ */
 public class Segment {
 
 	private Point3D a;
 	private Point3D b;
 	private boolean isVertical;
-	
-	public Segment (Point3D GpsA,Point3D GpsB) {
+
+	public Segment(Point3D GpsA, Point3D GpsB) {
 		this.a = GpsA;
 		this.b = GpsB;
-		if(GpsA.y()==GpsB.y()) {
+		if (GpsA.y() == GpsB.y()) {
 			this.isVertical = true;
-		}
-		else this.isVertical = false;
+		} else
+			this.isVertical = false;
 	}
-	
-		public String toString() {
+
+	public String toString() {
 		return "Segment [a=" + a + ", b=" + b + ", isVertical=" + isVertical + "]";
 	}
- /* Getters */
-	
-	
+	/* Getters */
+
 	public Point3D getA() {
 		return a;
 	}
@@ -34,8 +40,5 @@ public class Segment {
 	public boolean isVertical() {
 		return isVertical;
 	}
-
-
-	
 
 }
