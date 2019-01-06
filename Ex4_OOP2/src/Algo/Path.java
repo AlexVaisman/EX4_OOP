d2b103d823f182cdb57432c49220912756867dfd
shaynaor <44779960+shaynaor@users.noreply.github.com>
2019-01-06 11:16:44 +0200

    path doc

--- Ex4_OOP2/src/Algo/Path.java
+++ Ex4_OOP2/src/Algo/Path.java
@@ -5,12 +5,20 @@ import java.util.Iterator;
 
 import graph.Node;
 
+/**
+ * This class hold the way the player has to take to get to its target.
+ * This class can return the distance to the target and the id of the corners
+ * the play has to go to in Int.
+ * @author Alex vaisman, Shay naor
+ *
+ */
 public class Path {
 
 	private Node data;
 	private ArrayList<Integer> theWay;
 	private  double distance;
 
+	
 	public Path(Node data) {
 		this.data = data;
 		this.distance = data.getDist();
@@ -18,7 +26,9 @@ public class Path {
 		ConvertToInt();
 	}
 
-
+    /*
+     * This function converts corner ids from String to Int.
+     */
 	private void ConvertToInt() {
 		ArrayList<String> theStringWay = data.getPath();
 		Iterator<String> stringIt = theStringWay.iterator();
